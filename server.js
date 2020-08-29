be5d9caf3050b30ed08de157f6d7657c903da8e1
Jonas Lopes <jonasmassume@gmail.com>
2020-08-29 09:19:22 -0300

    Resolvendo problemas de direcionamento

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const nomeApp = process.env.npm_package_name;
+const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(`${__dirname}/dist/${nomeApp}`));
 
-app.listen(process.env.PORT || 8080);
-
-app.get('/*', function(req,res){
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+app.get('/*', (req, res) => {
+res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
 });
 
-console.log('Cosole listening!');
+app.listen(process.env.PORT || 8080);
