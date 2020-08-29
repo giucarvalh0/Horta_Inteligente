19783265acffb99d9f1a51eb100ef7a89c52f07d
Jonas Lopes <jonasmassume@gmail.com>
2020-08-29 09:33:59 -0300

    Resolvendo problemas de direcionamento 2

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
+
 const express = require('express');
 const path = require('path');
-const nomeApp = process.env.npm_package_name;
+//const nomeApp = process.env.npm_package_name;
 const app = express();
 
-app.use(express.static(`${__dirname}/dist/${nomeApp}`));
+app.use(express.static(`${__dirname}/dist/HortaInteligente`));
 
 app.get('/*', (req, res) => {
-res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
+res.sendFile(path.join(`${__dirname}/dist/HortaInteligente/index.html`));
 });
 
 app.listen(process.env.PORT || 8080);
+
+/*
+const express = require('express');
+const http = require('http');
+const app = express();
+const path = require('path');
+
+app.use(express.static(__dirname + '/dist/HortaInteligente'));
+
+
+
+app.get('/*', function(req,res){
+    res.sendFile(path.join(__dirname + '/dist/HortaInteligente/index.html'));
+});
+
+const server = http.createServer(app);
+server.listen(process.env.PORT || 8080);
+
+console.log('Console listening!');
+*/
