22c3c666921f566f9100e8a789ee677da8a5743c
Jonas Lopes <jonasmassume@gmail.com>
2020-09-07 18:48:17 -0300

    Adding routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import {LoginComponent} from './login/login.component';
+import {CadastroComponent} from './cadastro/cadastro.component';
+import {DashboardClienteComponent} from './dashboard-cliente/dashboard-cliente.component';
+import {DashboardCuidadorComponent} from './dashboard-cuidador/dashboard-cuidador.component';
+
+const routes: Routes = [{
+  path: "",
+  component: LoginComponent
+},
+{
+  path:"cadastro",
+  component: CadastroComponent
+},
+{
+  path:"dashboard",
+  component: DashboardClienteComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
